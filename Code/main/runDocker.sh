42aaec0f728588ddba3a39faf4bde7deb7a8ecfb
Pijus Krisiukenas <pijuskri@gmail.com>
2023-05-10 16:42:23 +0200

    trying to make docker work

--- Code/main/runDocker.sh
+++ Code/main/runDocker.sh
@@ -1,2 +1,7 @@
 #imageid=$(docker build -t mininet-rl -q .)
-docker run --rm -it $(docker build -t mininet-rl -q .)
+#docker run -u 0 --rm -it $(docker build -t mininet-rl -q .)
+#docker run -it --rm --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules $(docker build -t mininet-rl -q .)
+#docker run -it --rm --privileged $(docker build -t mininet-rl -q .)
+#docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock $(docker build -t mininet-rl -q .)
+
+docker run -it --rm --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules -v $(pwd):/data $(docker build -t mininet-rl -q .)
