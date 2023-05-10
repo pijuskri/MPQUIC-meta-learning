be0fca4db386aff4fbcae533ebda2d6828168b5d
Pijus Krisiukenas <pijuskri@gmail.com>
2023-05-10 21:17:04 +0200

    some more testing

--- Code/main/runDocker.sh
+++ Code/main/runDocker.sh
@@ -3,5 +3,7 @@
 #docker run -it --rm --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules $(docker build -t mininet-rl -q .)
 #docker run -it --rm --privileged $(docker build -t mininet-rl -q .)
 #docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock $(docker build -t mininet-rl -q .)
+#docker run -it --rm --net host --privileged -e DISPLAY -v $(pwd):/data $(docker build -t mininet-rl -q .) #testing
 
-docker run -it --rm --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules -v $(pwd):/data $(docker build -t mininet-rl -q .)
+docker run -it --rm --privileged -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules -v $(pwd):/data $(docker build -t mininet-rl -q .) #this works within wsl
+
