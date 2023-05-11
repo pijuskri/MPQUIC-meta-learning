be70737328f8ac3e3cde8557e82802eec6f67161
Pijus Krisiukenas <pijuskri@gmail.com>
2023-05-11 14:27:16 +0200

    shh works

--- Code/main/runDocker.sh
+++ Code/main/runDocker.sh
@@ -5,6 +5,7 @@
 #docker run --name containernet -it --rm --privileged --pid='host' -v /var/run/docker.sock:/var/run/docker.sock $(docker build -t mininet-rl -q .)
 #docker run -it --rm --net host --privileged -e DISPLAY -v $(pwd):/data $(docker build -t mininet-rl -q .) #testing
 
-docker run -it --rm --privileged -p 2223:22 -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules -v $(pwd):/data $(docker build -t mininet-rl -q .) #this works within wsl
+#to start in git bash, use 'winpty' before docker
+docker run -it --rm --privileged -p 2222:22 -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules -v $(pwd):/data $(docker build -t mininet-rl -q .) #this works within wsl
 #docker run -it --rm --privileged -p 2222:22 -e DISPLAY -v $(pwd)/mininet:/home/mininet -v /tmp/.X11-unix:/tmp/.X11-unix -v /lib/modules:/lib/modules -v $(pwd):/data $(docker build -t mininet-rl -q .)
 
